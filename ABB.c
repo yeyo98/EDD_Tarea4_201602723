4e5627ac7b8c73e6c5ef5e52952c350b927760aa
Diego Caballeros <ultimaster22@gmail.com>
2018-09-12 23:11:07 -0600

    Segundo Commit

--- ABB.c
+++ ABB.c
@@ -32,6 +32,71 @@ nodo* Agregar_Dato(nodo* dato, nodo* raiz){
     return raiz;
 }
 
+void PreOrden(){
+    Recorrer(raiz);
+}
+
+void Recorrer(nodo *raiz){
+
+    if(raiz != NULL){
+        printf("%d\n",raiz->numero);
+        Recorrer(raiz->izquierda);
+        Recorrer(raiz->derecha);
+    }
+}
+
+void Graficar(){
+    if(raiz != NULL){
+        char direccion[] = "ABB.dot";
+        fd = fopen(direccion,"wt");
+
+        if(fd == NULL){
+            printf("\nError al crear el archivo'n");
+            return;
+        }
+
+        //-------------- INICIO DEL CODIGO GRAPHVIZ -----------------
+        char begin[] = "digraph G {\n";
+        fputs(begin,fd);
+
+        Generar_Grafo(raiz);
+
+        fputs("}",fd);
+        fclose(fd);
+
+        system("dot -Tpng -O ABB.dot");
+        system("xdg-open ABB.dot.png");
+
+        //system("clear");
+    }
+}
+
+void Generar_Grafo(nodo *raiz){
+    if(raiz != NULL){
+    char derecha[500]; char izquierda[500];
+      if(raiz->izquierda != NULL){
+      sprintf(izquierda,"\"%d\" -> \"%d\"\n",raiz->numero,raiz->izquierda->numero);
+      fputs(izquierda,fd);
+      }
+
+      if(raiz->derecha != NULL){
+        sprintf(derecha,"\"%d\" -> \"%d\"\n",raiz->numero,raiz->derecha->numero);
+      fputs(derecha,fd);
+      }
+
+      Generar_Grafo(raiz->izquierda);
+      Generar_Grafo(raiz->derecha);
+    }
+
+}
+
+
+
+
+
+
+
+
 
 
 
--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include "ABB.h"
 
 int main(void)
 {
 
+    // 13-84-7-91-33-8-69-23
     int i;
-    for(i=0; i<5 ;i++){
+    for(i=0; i<8 ;i++){
         Insertar();
     }
+
+    PreOrden();
+
+    Graficar();
 
     return 0;
 }
